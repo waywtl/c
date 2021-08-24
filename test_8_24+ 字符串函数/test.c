06acbd065c76d1810eb93ff4e4886ecbbc3be78f
waywtl <1148117782@qq.com>
2021-08-24 23:13:47 +0800

    字符串函数

---
+++ test_8_24+ 字符串函数/test.c
@@ -0,0 +1,102 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+//strlen
+ 
+//1.计数器的方法
+//2.递归
+//3.指针-指针
+
+//1.
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str)
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//size_t == unsigned int
+
+//int main()
+//{
+//	if (strlen("abc") - strlen("abcdef") > 0)
+//	{
+//		printf("hehe\n");
+//	}
+//	else
+//	{
+//		printf("haha\n");
+//	}
+//
+//	return 0;
+//}
+
+//长度不受限制的字符串函数
+
+//strcpy
+
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	char* ret = dest;
+//	//拷贝src指向的字符串到dest指向的空间，包含'\0'
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	//返回目的空间的起始地址
+//	return ret;
+//}
+
+//strcat
+
+//char* my_strcat(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//
+//	//1.找到目的字符串的'\0'
+//	while (dest != '\0')
+//	{
+//		dest++;
+//	}
+//	//2.追加
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+
+//strcmp
+
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 != NULL);
+//	assert(str2 != NULL);
+//	//比较
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')
+//		{
+//			return 0;//相等
+//		}
+//		str1++;
+//		str2++;
+//	}
+//	if (*str1 > *str2)
+//		return 1;//大于
+//	else
+//		return -1;//小于
+//}
+
